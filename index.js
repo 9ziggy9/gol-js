991b3bce2c1cacb3b3623bcfb37f338fa44a0b31
9ziggy9 <davidarogers@protonmail.com>
2022-07-20 14:22:40 -0700

    debug button and neighborcounting

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const DIRS = [
+  [1,0], // right
+  [-1,0], // left
+  [0,1], // down
+  [0,-1], // up
+  [1,-1], // top right
+  [-1,-1], // top left
+  [-1,1], // bottom left
+  [1,1], // bottom right
+];
+  
 function initialize() {
   buildGrid();
   initButtons();
@@ -7,13 +18,27 @@ const randColor = () => "#" + [...Array(6)]
       .map(n => Math.floor(Math.random() * 16).toString(16))
       .join("");
 
+const isAlive = (x,y) => document.getElementById(`${x},${y}`)
+      .classList.contains("alive");
+
+function countNeighbors(x,y) {
+  console.log(`counting neighbors of ${x},${y}`);
+  return DIRS.reduce((count, d) => count + (isAlive(x+d[0],y+d[1]) ? 1 : 0), 0);
+}
+
+function fnDebug() {
+  console.log(countNeighbors(8,6));
+}
+
 function initButtons() {
   const start = document.getElementById("start");
   const clear = document.getElementById("clear");
+  const debug = document.getElementById("debug");
   start.addEventListener("click", () => start.classList.contains("off")
 			 ? start.setAttribute("class", "on")
 			 : start.setAttribute("class", "off"));
   clear.addEventListener("click", () => clearGrid());
+  debug.addEventListener("click", () => fnDebug());
 }
 
 function clearGrid() {
